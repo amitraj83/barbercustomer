df390786440424bafb3b72192cb3e9b11c87bf5b
BlackGold <bgold1118@gmail.com>
2019-10-24 02:44:53 +0700

    adding customer page

---
+++ app/src/main/java/com/amit/barberc/fragment/BarberDetailFragment.java
@@ -0,0 +1,28 @@
+package com.amit.barberc.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.amit.barberc.R;
+
+public class BarberDetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        int position = FragmentPagerItem.getPosition(getArguments());
+//        TextView title = (TextView) view.findViewById(R.id.item_title);
+//        title.setText(String.valueOf(position));
+    }
+}
